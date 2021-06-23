f5ed98075168e93d2fc813dc1ed2227501cbf05d
unknown <schmidt1519@gmail.com>
2021-06-23 16:01:48 -0500

    Created comment and reply serializers. Working on views methods next.

---
+++ comments/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Comment
+from .models import Reply
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'video_id', 'comment_text', 'like', 'dislike']
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['id', 'comment', 'reply_text']
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Comment
+from .models import Reply
+from .serializers import CommentSerializer
+from .serializers import ReplySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
