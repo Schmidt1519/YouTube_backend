c28c64cabd1f336a551d5e8e6cf79c39c192b5cc
unknown <schmidt1519@gmail.com>
2021-06-23 16:15:57 -0500

    Updated views import and created CommentList class and the get method for it.

--- comments/views.py
+++ comments/views.py
@@ -6,6 +6,17 @@ from .serializers import ReplySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
 
 # Create your views here.
+class CommentList(APIView):
+
+    def get(self, request):
+        try:
+            comment = Comment.objects.all()
+            serializer = CommentSerializer(comment, many=True)
+            return Response(serializer.data)
+        except Comment.DoesNotExist:
+            raise Http404
+
