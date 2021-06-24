e963b2da5b2e9f55beeb8db11c09627fdf67bbdb
unknown <schmidt1519@gmail.com>
2021-06-24 09:15:24 -0500

    Added ReplyList and ReplyDetail classes to views. Added paths for ReplyList and ReplyDetail to urls. Tested all in Postman.

--- comments/urls.py
+++ comments/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
     path('comments/<int:pk>/', views.CommentDetail.as_view()),
+    path('reply/', views.ReplyList.as_view()),
+    path('reply/<int:pk>/', views.ReplyDetail.as_view()),
 ]
--- comments/views.py
+++ comments/views.py
@@ -62,3 +62,48 @@ class CommentDetail(APIView):
         comment = self.get_object(pk)
         comment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ReplyList(APIView):
+    def get(self, request):
+        try:
+            reply = Reply.objects.all()
+            serializer = ReplySerializer(reply, many=True)
+            return Response(serializer.data)
+        except Reply.DoesNotExist:
+            raise Http404
+
+    def post(self, request):
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReplyDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Reply.objects.get(pk=pk)
+        except Reply.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        reply = self.get_object(pk)
+        serializer = ReplySerializer(reply)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        comment = self.get_object(pk)
+        serializer = ReplySerializer(comment, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        reply = self.get_object(pk)
+        reply.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
